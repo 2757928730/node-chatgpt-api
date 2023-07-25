f16c173fd76b0b4d0bf39c93ac071d56a70093ca
yangbm <2757928730@qq.com>
2023-07-25 23:35:02 +0800

    适配2

--- src/ChatGPTClient.js
+++ src/ChatGPTClient.js
@@ -1,7 +1,6 @@
 import './fetch-polyfill.js';
 import crypto from 'crypto';
 import Keyv from 'keyv';
-import { encoding_for_model as encodingForModel, get_encoding as getEncoding } from '@dqbd/tiktoken';
 import { fetchEventSource } from '@waylaidwanderer/fetch-event-source';
 import { Agent, ProxyAgent } from 'undici';
 
